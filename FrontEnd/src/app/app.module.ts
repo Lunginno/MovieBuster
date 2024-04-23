6284af31a5faeaec3f1d287d46c6aafe6be2b5fc
Sdeezy99 <mphomadini8@gmail.com>
2024-04-23 16:17:07 +0200

    Initial commit

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WatchlistComponent } from './pages/watchlist/watchlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WatchlistComponent
   ],
   imports: [
     BrowserModule,
