a38d48d5023c38a4922512fc0e93e4227a94bba0
Sdeezy99 <mphomadini8@gmail.com>
2024-04-24 11:14:29 +0200

    Watchlist

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WatchlistComponent } from './pages/watchlist/watchlist.component';
+import { CardsComponent } from './components/cards/cards.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WatchlistComponent
+    WatchlistComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
