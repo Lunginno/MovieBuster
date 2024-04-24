e29d37cf110a4a61bffb21927964026c5fcbe90f
Shero25 <Sherolinenthabiseng@gmail.com>
2024-04-24 16:32:03 +0200

    added files

--- FrontEnd/src/app/pages/signin/signin.component.ts
+++ FrontEnd/src/app/pages/signin/signin.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent {
+public signIn !: FormGroup
 
+constructor(private http: HttpClient, private router:Router, private formBuilder: FormBuilder){}
+
+ngOnInit():void{
+  this.signIn=this.formBuilder.group({
+    email:[""],
+    password:[""]
+  })
 }
+
+signInForm()
+{
+  this.http.get<any>("http://localhost:3000/signupUsersList").subscribe(resp=>{
+    const user=resp.find((value:any)=>{
+      
+    })
+  })
+}
+}
+
--- FrontEnd/src/app/pages/signup/signup.component.ts
+++ FrontEnd/src/app/pages/signup/signup.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-signup',
@@ -7,4 +11,34 @@ import { Component } from '@angular/core';
 })
 export class SignupComponent {
 
+  //signup will bw a formgroup object
+  public signUp !: FormGroup
+  constructor(private formBuilder: FormBuilder, private http: HttpClient, private router: Router){}
+
+  ngOnInit():void
+  {
+    //assigns the form group created by formbuilder.group() to the sigUp
+   this.signUp = this.formBuilder.group({
+      email: [''],
+      password: ['']
+
+    })
+  }
+
+  signup()
+  {
+    this.http.post<any>("http://localhost:3000/signupUsersList/",this.signUp.value).subscribe(resp=>{
+
+      console.log('sign up successful');
+      //the navigator method accepts an array of route as an argument
+      console.log(this.signup());
+      
+      // this.router.navigate(["login"])
+    },error=>{
+        alert("something went wrong");
+
+    })
+    //send a request to the server
+    
+  }
 }
