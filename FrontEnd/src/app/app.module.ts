f3965f30bbc075eb47340a314754f6be3b930fde
Lunginno <sifumbalungile@gmail.com>
2024-04-24 14:45:33 +0200

    navbar

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { WatchlistComponent } from './pages/watchlist/watchlist.component';
 import { CardsComponent } from './components/cards/cards.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WatchlistComponent,
     CardsComponent,
-    HomePageComponent
+    HomePageComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
