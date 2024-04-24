4517bebd2c1b5d38b17df4efd70da4720f377eb9
Asanda3 <fanele351@gmail.com>
2024-04-24 16:16:51 +0200

    Profile

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
