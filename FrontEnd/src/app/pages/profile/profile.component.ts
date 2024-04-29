e90ef2e3a394ec7e56e54d20b7aacad3bac102c6
Lunginno <sifumbalungile@gmail.com>
2024-04-29 11:09:59 +0200

    changes

--- FrontEnd/src/app/pages/profile/profile.component.ts
+++ FrontEnd/src/app/pages/profile/profile.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  signin: any;
 
+  constructor(private http: HttpClient,private signinserv :AuthService) {}
+
+  ngOnInit() {
+    this.signinserv.signIn().subscribe((data: any) => {
+      this.signin = data;
+      // console.log(this.signin[0].email);
+    });
+  }
 }
