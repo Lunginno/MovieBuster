2beb558f873518ebe37e39b1cc0c102c332432f8
Duzzii <mdundoro@gmail.com>
2024-05-14 15:16:21 +0200

    Spring Boot backend is created

---
+++ backend /src/main/java/com/moviebuster/moviebuster/controller/MovieController.java
@@ -0,0 +1,30 @@
+package com.moviebuster.moviebuster.controller;
+
+//import org.springframework.web.bind.annotation.;
+import com.moviebuster.moviebuster.entity.Movies;
+import com.moviebuster.moviebuster.service.MovieService;
+import com.moviebuster.moviebuster.service.MovieServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+
+    @Autowired
+    private MovieServiceImpl movieService;
+
+    @GetMapping
+    public List<Movies> findall(){
+        return movieService.getAllMovies();
+    }
+
+    @PostMapping
+    public void saveMovie(@RequestBody Movies movie){
+        movieService.saveMovie(movie);
+    }
+
+
+}
