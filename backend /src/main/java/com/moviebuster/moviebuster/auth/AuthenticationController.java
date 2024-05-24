37b10364034ce1ba04b45b2b7c0a314f6b9a44ab
Noorun29 <abdullahnoorun@gmail.com>
2024-05-24 15:49:41 +0200

    changes

--- backend /src/main/java/com/moviebuster/moviebuster/auth/AuthenticationController.java
+++ backend /src/main/java/com/moviebuster/moviebuster/auth/AuthenticationController.java
@@ -3,10 +3,7 @@ package com.moviebuster.moviebuster.auth;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -16,6 +13,7 @@ public class AuthenticationController {
 
     private final AuthenticationService service;
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
             @RequestBody RegisterRequest request
